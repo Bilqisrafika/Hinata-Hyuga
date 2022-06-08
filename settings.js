53c0f3fee72cf84e0cb68f89d4dc86a729ab7c97
X - XDZ <104031305+Bilqisrafika@users.noreply.github.com>
2022-06-08 18:40:34 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,18 +23,18 @@
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
-  global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
-  global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
+  global.ownername = 'Exs - Botz' // Ganti Jadi Namamu
+  global.owner = ['6285334190704','6285334190704','6285334190704'] // Kalo Mau Lebih Tambahin Aja
+  global.ownernomer = '6285334190704' // Owner Utama Istilahnya
+  global.botname = 'Exs - Botz' // Ganti Jadi Nama Botmu
+  global.packname = 'Exs - Botz' // Sticker Weem
+  global.author = 'Exs - Ditz' // Sticker Weem
+  global.webme = 'https://desty.page/Ochoofficial' // Ganti Sama Web Mu
+  global.linkgroup = 'https://chat.whatsapp.com/Dupj9RhYXykDOAAiQoM0GG' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.youtube = 'https://youtube.com/channel/UCjdHhl4_n8k9s4I90fvTx0g' // Ganti Sama Link Yt Mu
+  global.github = 'https://github.com/Bilqisrafika' // Ganti Sama Link Github Mu
+  global.instagram = 'https://www.instagram.com/aditadwir_/' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
